cd7b644c9fe3af871e275f40938a595b9ead1dd7
Chaimaa  Elot <elot10892@gmail.com>
2026-01-18 00:13:17 +0100

    Update API base URL

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: import.meta.env.VITE_API_URL, // ici on utilisera la variable d'environnement
     headers: {
         "Content-Type": "application/json",
     },
